555de49064454c8b5bb34094dab84e8890102961
Bigvinn23 <bigvinn23@gmail.com>
2022-05-01 20:39:42 -0400

    other things

--- src/DataBar.java
+++ src/DataBar.java
@@ -1,5 +1,5 @@
 /*
-ClassName: MainFrame
+ClassName: DataBar
 Author: Jamaine Drakes
 Purpose: 
 Start Date: May 01, 2022
@@ -12,79 +12,40 @@ Last Edit: May 01, 2022
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
-import javax.xml.crypto.Data;
-
-import java.util.ArrayList;
+import javax.swing.plaf.DimensionUIResource;
 
 
-public class MainFrame extends JFrame implements ActionListener
+public class DataBar extends JPanel
 {
     //========================================================================================//
     //                                    DATA MEMBERS                                        //
     //========================================================================================//
-    int maxHeight;
-    int minHeight;
-    int numDataBars;
-
-    ArrayList<DataBar> dataBar;
+    private int height;
     
-    public static void main(String[] args)
-    {
-        new MainFrame();
-    }// main
-
-
     //========================================================================================//
     //                                    CONSTRUCTOR                                         //
     //========================================================================================//
-    public MainFrame()
+    public DataBar(int newHeight)
     {
+        height = newHeight;
+
         setLayout(new FlowLayout());
-        setSize(500, 400);
-        setTitle("Title Here");
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        centerFrame(this);
+        //setSize(500, height);
 
-        maxHeight = this.getHeight();
-        minHeight = 50;
-        numDataBars = 5;
-
-        dataBar = new ArrayList<DataBar>();
+        setBackground(Color.BLACK);
         
-        int currentHeight;
-        int heightDiff;
-
-        heightDiff = maxHeight - minHeight;
-        currentHeight = minHeight;
-
-        int heightJump;
-
-        heightJump = heightDiff / numDataBars;
         
         //========================================================================================//
         //                                  ADDING COMPONENTS                                     //
         //========================================================================================//
-
-        for(int i = 0; i < numDataBars; i++)
-        {
-            dataBar.add(new DataBar(currentHeight));
-
-            currentHeight += heightJump;
-        }
-
-        for(int i = 0; i < dataBar.size(); i++)
-        {
-            add(dataBar.get(i));
-
-            currentHeight += heightJump;
-        }
+    
     
         //========================================================================================//
         //                                ADDING ACTION LISTENERS                                 //
         //========================================================================================//
     
-        setVisible(true);
-    }// MainFrame
+    
+    }// DataBar
 
     //========================================================================================//
     //                                ACTION PERFORMED METHOD                                 //
@@ -99,22 +60,13 @@ public class MainFrame extends JFrame implements ActionListener
     //========================================================================================//
     //                                    OTHER METHODS                                       //
     //========================================================================================//
-    
-    
-    // This method will center the frame on the screen
-    private static void centerFrame(JFrame fr)
+    @Override
+    public Dimension getPreferredSize() 
     {
-        // Get the size of the screen
-        Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+        // TODO Auto-generated method stub
+        int width = super.getPreferredSize().width;
 
-        // Determine the new location of the window
-        int w = fr.getSize().width;
-        int h = fr.getSize().height;
-        int x = (dim.width - w) / 2;
-        int y = (dim.height - h) / 2;
-        // Move the window
-        fr.setLocation(x, y);
+        return new Dimension(width, height);
+    }
 
-    }// centerFrame
-
-}// MainFrame
+}// DataBar
